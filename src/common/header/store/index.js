6cd114082368dd26642a4aec220123d9d651673b
liChenYang <chenyang.li@dmall.com>
2019-04-15 21:27:48 +0800

    combineReducers 拆分 reducer

---
+++ src/common/header/store/index.js
@@ -0,0 +1,13 @@
+/**
+ * @Desc:
+ * @Usage:
+ * @Notify:
+ * @depend:
+ *
+ *
+ * Created by aYang on 2019-04-15
+ */
+
+import reducer from './reducer'
+
+export {reducer}
--- src/store/index.js
+++ src/store/index.js
@@ -7,9 +7,13 @@
  *
  * Created by aYang on 2019-04-15
  */
-import {createStore} from "redux";
+import {createStore, compose, applyMiddleware} from "redux";
 import reducer from './reducer';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer,composeEnhancers(
+	applyMiddleware()
+));
 
 export default store;
