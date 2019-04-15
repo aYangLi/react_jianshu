0b73edcda2075a973ab27993a834d4438d1adec6
liChenYang <chenyang.li@dmall.com>
2019-04-15 11:14:26 +0800

    简书头部布局

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -9,14 +9,24 @@
  */
 
 import React, {Component} from 'react';
-import { HeaderWrap, Logo, Nav } from './style';
+import { HeaderWrap, Logo, Nav, NavItem, NavSearch, Addition  ,Buttion} from './style';
 
 class Header extends Component {
 	render() {
 		return  (
 			<HeaderWrap>
 				<Logo></Logo>
-				<Nav></Nav>
+				<Nav>
+					<NavItem className='left active'>首页</NavItem>
+					<NavItem className='left'>下载App</NavItem>
+					<NavItem className='right'>登录</NavItem>
+					<NavItem className='right'>Aa</NavItem>
+					<NavSearch></NavSearch>
+				</Nav>
+				<Addition>
+					<Buttion className='writing'>写文章</Buttion>
+					<Buttion className='reg'>注册</Buttion>
+				</Addition>
 			</HeaderWrap>
 		)
 	}
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -33,6 +33,67 @@ export const Logo = styled.a.attrs({
 export const Nav = styled.nav`
 	width: 960px;
 	height: 100%;
+	box-sizing: border-box;
+	padding-right: 70px;
 	margin: 0 auto;
-	background: blue;
+`;
+
+export const NavItem = styled.div`
+	line-height: 56px;
+	padding: 0 15px;
+	font-size: 17px;
+	color: #333;
+	&.left {
+		float: left;
+	}
+	&.right {
+		float: right;
+		color: #969696;
+	}
+	&.active {
+		color: #ea6f5a;
+	}
+`;
+
+export const NavSearch = styled.input.attrs({
+	placeholder: '搜索'
+})`
+	width: 160px;
+	height: 38px;
+	padding: 0 20px;
+	margin-top: 9px;
+	box-sizing: border-box;
+	background: #eee;
+	border: none;
+	outline: none;
+	font-size: 14px;
+	border-radius: 19px;
+	&::placeholder {
+	 color: #999;
+	}
+`;
+
+export const Addition = styled.div`
+	position: absolute;
+	right: 0;
+	top: 0;
+	height: 56px;
+`;
+
+export const Buttion = styled.div`
+	float:right;
+	line-height: 38px;
+	margin-top: 9px;
+	border-radius: 19px;
+	border: 1px solid #ec6149;
+	margin-right: 20px;
+	padding: 0 20px;
+	font-size: 14px;
+	&.reg{
+		color: #ec6149;
+	}
+	&.writing {
+		color: #fff;
+		background: #ec6149;
+	} 
 `;
