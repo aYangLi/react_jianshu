5708dd75f24d6779b0a9a4e0fc1ac1c5ab1c58fc
liChenYang <chenyang.li@dmall.com>
2019-04-16 11:47:30 +0800

    引入immutable设置state

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -9,20 +9,20 @@
  */
 import {SEARCH_FOCUS, SEARCH_BLUR} from "./constants";
 
-const  defaultState = {
+import {fromJS} from "immutable";
+
+const  defaultState = fromJS({
 	focused: false,
-};
+});
 
 export default (state = defaultState, action) => {
 	if (action.type === SEARCH_FOCUS) {
-		return  {
-			focused :true,
-		}
+		// immutable 对象的 set 方法，会结合之前immutable对象的值
+		// 和设置的值，会返回一个全新的对象
+		return state.set('focused', true)
 	}
 	if (action.type === SEARCH_BLUR) {
-		return  {
-			focused :false,
-		}
+		return state.set('focused', false);
 	}
 	return state;
 }
