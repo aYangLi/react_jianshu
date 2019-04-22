b318bbaeec5da395387a11dd74d9496b3a0dcab1
liChenYang <chenyang.li@dmall.com>
2019-04-22 09:06:42 +0800

    打包上传

---
+++ build/precache-manifest.283081a17ad58240f5a918c8a82a0fb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fecf84efbc67d0aebd2",
+    "url": "/static/js/runtime~main.35e653f0.js"
+  },
+  {
+    "revision": "b369201cd3c50f8942b7",
+    "url": "/static/js/main.fe4faf30.chunk.js"
+  },
+  {
+    "revision": "6f7829a5077e19dc151f",
+    "url": "/static/js/3.221b674a.chunk.js"
+  },
+  {
+    "revision": "69f6eba2ec457375064f",
+    "url": "/static/js/2.d1e96ea7.chunk.js"
+  },
+  {
+    "revision": "5d281cfed9c29404706476fa9bfe409a",
+    "url": "/index.html"
+  }
+];
